c441fcf8c1aba86561edcd9614ad659866854aad
mike-uffelman <muffelma@gmail.com>
2022-01-14 12:15:17 -0500

    updating config resources to https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const FORECAST_URL = `https://api.openweathermap.org/data/2.5/`
-export const GEOCODE_REVERSE_URL = `http://api.openweathermap.org/geo/1.0/reverse`;
-export const GEOCODE_DIRECT_URL = `http://api.openweathermap.org/geo/1.0/direct`;
+export const GEOCODE_REVERSE_URL = `https://api.openweathermap.org/geo/1.0/reverse`;
+export const GEOCODE_DIRECT_URL = `https://api.openweathermap.org/geo/1.0/direct`;
 
